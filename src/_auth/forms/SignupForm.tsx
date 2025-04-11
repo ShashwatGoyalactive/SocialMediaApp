c57be529a7124df289f9bdd70a27d2fb34a33417
Shashwat <shashwatgoyal2004@gmail.com>
2025-04-11 08:40:12 +0530

    first commit -m

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Sign up</Button>
+    </div>
+  )
+}
+
+export default SignupForm
