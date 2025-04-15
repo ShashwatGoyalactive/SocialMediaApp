bef7a6dbe8fe8079f2e23b18b13e2c0f046726ea
Shashwat <shashwatgoyal2004@gmail.com>
2025-04-15 06:02:11 +0530

    feat: implement Home page with recent posts display; add PostCard component and loading state

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -9,11 +9,12 @@ import { INewPost, INewUser } from "@/types";
 import {
   createPost,
   createUserAccount,
+  getRecentPosts,
   signInAccount,
   signOutAccount,
 } from "@/lib/appwrite/api";
 
-
+import { QUERY_KEYS } from "./queryKeys";
 // Initalize a new mutation function so that React query knows when we are creating a user
 
 export const useCreateAccount = () => {
@@ -33,12 +34,22 @@ export const useSignOutAccount = () => {
   return useMutation({ mutationFn: signOutAccount });
 };
 
-
 export const useCreatePost = () => {
- 
+  const queryClient = useQueryClient();
 
-  return  useMutation({
+  return useMutation({
     mutationFn: (post: INewPost) => createPost(post),
-    
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      }),
   });
+};
+
+
+export const useGetRecentPosts = ()=> {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  })
 }
